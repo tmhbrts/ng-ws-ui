8be252bdc989c323f7cbe5d8283abf1fdbd35c4e
Tim Huiberts <tmhbrts@gmail.com>
2019-09-24 11:55:34 +0200

    add light switch example to app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,19 @@ import { WebSocketService } from './web-socket.service'
 })
 export class AppComponent implements OnInit {
   title = 'ng-ws-ui';
+  switchState: boolean = false;
+
+  toggleSwitch = () => {
+    this.switchState ? this.switchOn() : this.switchOff();
+  }
+
+  switchOn = () => {
+    this.webSocket.send('light switch', 'on');
+  }
+
+  switchOff = () => {
+    this.webSocket.send('light switch', 'off');
+  }
 
   ngOnInit() {
     this.webSocket.connect();
